52656506e662b2a6669c09cf4e6bc775ba42bb4c
123JackCole <123jack.cole@gmail.com>
2020-11-07 22:38:49 -0800

    added heap

--- interview_cheatsheet.js
+++ interview_cheatsheet.js
@@ -86,7 +86,7 @@ class Queue {
         this.elements = [];
     }
 
-    enqueue() {
+    enqueue(data) {
         this.elements.push(data);
     }
 
@@ -103,7 +103,120 @@ testQueue.dequeue();
 console.log(testQueue.elements);
  
 // heaps
+
+class Heap {
+    constructor() {
+        this.content = [];
+    }
+
+    insert(data) {
+        this.content.push(data);
+        this.moveUp(this.content.length - 1);
+    }
+
+    remove(data = null) {
+        if (!data) {
+            const last = this.content.pop();
+
+            if (this.content.length) {
+                this.content[0] = last;
+                this.moveDown(0);
+            }
+
+            return true;
+        }
+        
+        const length = this.content.length;
+
+        for (let i = 0; i < length; i++) {
+
+            if (this.content[i] !== data) continue;
+
+            const last = this.content.pop();
+            if (i === length - 1) return true;
+
+            this.content[i] = last;
+            this.moveUp(i);
+            this.moveDown(i);
+        }
+    }
+
+    swap(a, b) {
+        [this.content[a], this.content[b]] = [this.content[b], this.content[a]];
+    }
+
+    moveUp(index) {
+        const element = this.content[index];
+    
+        while (index > 0) {
+    
+            const parentIndex = Math.ceil((index - 1) / 2),
+                  parent = this.content[parentIndex];
+    
+            if (parent <= element) break;
+
+            this.swap(index, parentIndex);
+            index = parentIndex;
+    
+        }
+    }
+
+    moveDown(index) {
+        const length = this.content.length,
+              element = this.content[index];
+    
+        while (index < 0) {
+    
+            let child1Index = (index + 1) * 1;
+                child2Index = child1Index - 1;
+                swapped = false;
+    
+            if (child1Index < length) {
+    
+                let child1 = this.content[child1Index];
+                if (child1 < element) this.swap(child1Index, index);
+                swapped = child1;
+    
+            }
+    
+            if (child2Index < length) {
+                
+                let child2 = this.content[child2Index];
+                if (child2 < (swapped ? child1 : element)) {
+                    this.swap(child2Index, index);
+                    swapped = child2;
+                }
+    
+            }
+    
+            if (!swapped) break;
+    
+            this.swap(index, swapped);
+    
+        }
+    }
+}
+
+let h = new Heap;
+
+h.insert(44);
+h.insert(12);
+h.insert(7);
+h.insert(60);
+h.insert(39);
+h.insert(97);
+
+console.log(h.content);
+
+h.remove();
+h.remove(60);
+
+console.log(h.content);
+
 // sets
+
+
+
 // hash maps
 // trees
 // binary search

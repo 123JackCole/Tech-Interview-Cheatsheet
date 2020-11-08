7c26a3f0a01b086b2e4ae7a13b9cea030a336bd7
123JackCole <123jack.cole@gmail.com>
2020-11-07 21:49:03 -0800

    added singly linked list

--- interview_cheatsheet.js
+++ interview_cheatsheet.js
@@ -1,10 +1,70 @@
+// iteration
+// recursion
+// arrays
+// nested arrays (matricies)
+
+// linked lists
+class Node {
+
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+
+}
+
+class LinkedList {
+
+    constructor() {
+        this.head = null;
+    }
+
+    add(data) {
+        const node = new Node(data);
+        if (!this.head) {
+            this.head = node;
+            return this.head;
+        }
+
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = node;
+
+        return this.head;
+    }
+
+    print() {
+        let current = this.head;
+        while (current.next) {
+            console.log(current.data);
+            current = current.next;
+        }
+        console.log(current.data);
+    }
+
+}
+
+ll = new LinkedList();
+ll.add(3);
+ll.add(2);
+ll.add(4);
+ll.add(1);
+ll.print();
+
 // stacks
 // queues
-// memoization
-// recursion
+// heaps
+// sets
+// hash maps
+// trees
+// binary search
+// dfs
+// bfs
+// tries
 // merge sort
 // quick sort
-// trees
-// bfs, dfs
+// memoization
 
 // combine all past blogs into this post
